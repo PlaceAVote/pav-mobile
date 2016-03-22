30b3a8d6fa51744f34509fb8ce3fe0af0976405d
John Kokkinidis <sudoplz@gmail.com>
2016-03-22 16:46:24 +0200

    First commit placeavote  app.

---
+++ src/config/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  PAV_BACKEND: {
+    API_BASE_URL: '',
+    APP_ID: '',
+    REST_API_KEY: '',
+    SESSION_TOKEN_KEY: 'SESSION_TOKEN_KEY'
+  },
+  backend: {
+    pav: true,
+    parse: false,
+    hapiRemote: false,
+    hapiLocal: false
+  }
+}
