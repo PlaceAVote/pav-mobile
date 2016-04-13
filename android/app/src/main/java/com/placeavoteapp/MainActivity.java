9a8219496e3311c07cd658d0465199a5038d0976
John Kokkinidis <sudoplz@gmail.com>
2016-04-13 18:03:02 +0300

    Configured the Android project for fb sdk.

--- android/app/src/main/java/com/placeavoteapp/MainActivity.java
+++ android/app/src/main/java/com/placeavoteapp/MainActivity.java
@@ -1,6 +1,12 @@
 package com.placeavoteapp;
 
-import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivity;   //for FacebookSdk;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;   //for FacebookSdk;
+import com.facebook.CallbackManager;   //for FacebookSdk;
+import com.facebook.FacebookSdk;   //for FacebookSdk;
+
+
+
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.github.yamill.orientation.OrientationPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -11,6 +17,8 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends ReactActivity {
+
+    CallbackManager mCallbackManager;   //for FacebookSdk;
 
     /**
      * Returns the name of the main component registered from JavaScript.
@@ -36,11 +44,26 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected List<ReactPackage> getPackages() {
+        mCallbackManager = new CallbackManager.Factory().create();
+
         return Arrays.<ReactPackage>asList(
             new MainReactPackage(),
+            new FBSDKPackage(mCallbackManager),
             new LinearGradientPackage(),
             new OrientationPackage(this),
             new VectorIconsPackage()
         );
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+    } 
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
